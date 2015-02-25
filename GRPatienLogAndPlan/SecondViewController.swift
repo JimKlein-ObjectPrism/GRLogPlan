d3161dbaab40547ff32e056b76bbd045107ac467
Jim Klein <pcsstudent@PCSs-MacBook-Pro-2.local>
2015-02-25 13:41:18 -0800

    Initial Commit

---
+++ GRPatienLogAndPlan/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GRPatienLogAndPlan
+//
+//  Created by Jim Klein on 2/25/15.
+//  Copyright (c) 2015 ObjectPrism Corp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
