ba36f17e06c71ae2e6e9eabde5c3905e392ba513
James Klein <JimKlein@Jamess-MacBook-Pro.local>
2015-04-28 18:14:52 -0700

    Several new views.  Code to enable navigation.

--- GRPatienLogAndPlan/AppDelegate.swift
+++ GRPatienLogAndPlan/AppDelegate.swift
@@ -56,7 +56,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let homeNaveController = storyboard.instantiateViewControllerWithIdentifier("HomeNavController") as UINavigationController
         
         //let homeViewController = homeNaveController.viewControllers[0] as HomeViewController
-    
+        
+        let profileTableVC =  storyboard.instantiateViewControllerWithIdentifier("ProfileTableViewController") as ProfileTableViewController
+        let profileNav = storyboard.instantiateViewControllerWithIdentifier("ProfileNavController") as UINavigationController
+        
+        let printTableVC =  storyboard.instantiateViewControllerWithIdentifier("PrintViewController") as PrintViewController
+        let printNavVC = storyboard.instantiateViewControllerWithIdentifier("PrintViewNavController") as UINavigationController
         
         // set up Tab Bar Controller
         let tabBarController = HomeTabBarController()
@@ -64,7 +69,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         tabBarController.viewControllers?.removeAll(keepCapacity: false)
         
         // tab bar target view controllers added in order here:
-        let controllers = [ homeNaveController , trackController ]
+        let controllers = [ homeNaveController , trackController , printNavVC, profileNav ]
         //let controllers = [ homeNaveController , detailTableViewController, trackController , menuTableViewController]
 
         tabBarController.viewControllers = controllers
@@ -72,8 +77,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // set tab bar icon and text here
         trackController.tabBarItem =  UITabBarItem(title: "Track", image: UIImage(named: "calendar_day-16"), selectedImage: nil)
         homeNaveController.tabBarItem =  UITabBarItem(title: "Home", image: UIImage(named: "home-16"), selectedImage: nil)
-//        detailTableViewController.tabBarItem =  UITabBarItem(title: "Today", image: UIImage(named: "Track-16"), selectedImage: nil)
-//        menuTableViewController.tabBarItem =  UITabBarItem(title: "Settings", image: UIImage(named: "profile_user-16"), selectedImage: nil)
+        
+        printNavVC.tabBarItem =  UITabBarItem(title: "Print", image: UIImage(named: "print-16"), selectedImage: nil)
+        profileNav.tabBarItem =  UITabBarItem(title: "Profile", image: UIImage(named: "profile_user-16"), selectedImage: nil)
 //        
         window?.rootViewController = tabBarController
         return true
